ac38a858daac607f96624cbed8a03982260499ef
codewithyasho <yashodeephundiwale@gmail.com>
2026-08-19 19:25:47 +0530

    Add packages.txt for ffmpeg and ffprobe system dependencies on Streamlit Cloud

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -1,8 +1,13 @@
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+try:
+    from langchain.chains.combine_documents import create_stuff_documents_chain
+    from langchain.chains import create_retrieval_chain
+except ImportError:
+    from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+    from langchain_classic.chains import create_retrieval_chain
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_classic.chains import create_retrieval_chain
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
